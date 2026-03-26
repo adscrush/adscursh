c01788698f6f3cecbe88b1997d7c3e147a899fac
Anonymous Sherlock <anonymoushsherlock@gmail.com>
2026-03-26 18:51:44 +0530

    refactor(ui): rename package scope to @adscrush and add new UI components
    - Changed package name and import aliases from @workspace/ui to @adscrush/ui
    - Updated eslint config import path to @adscrush/eslint-config/react-internal
    - Added new components: Accordion, AlertDialog, Alert, AspectRatio, Avatar, Badge
    - Added Breadcrumb, ButtonGroup, Calendar, Card, Carousel components
    - Updated dependencies and devDependencies in package.json for new packages and workspace references
    - Ensured consistent utility function import from @adscrush/ui/lib/utils instead of @workspace/ui/lib/utils

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from "@workspace/eslint-config/react-internal"
+import { config } from "@adscrush/eslint-config/react-internal"
 
 /** @type {import("eslint").Linter.Config} */
 export default config
